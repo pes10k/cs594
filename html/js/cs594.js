b4d05d21bd68bbf9b50ac9c1cc73518131c575b5
Peter Snyder <snyderp@gmail.com>
2014-02-21 17:36:08 -0600

    initial HTML pages, results-to-json converter

---
+++ html/js/cs594.js
@@ -0,0 +1,146 @@
+(function () {
+
+    var bytesToSize;
+
+    // Set up simple skelleton for expected "namespaces"
+    window.cs594 = {
+        data: {},
+    };
+
+    bytesToSize = function (bytes, precision) {
+        var kilobyte = 1024;
+        var megabyte = kilobyte * 1024;
+        var gigabyte = megabyte * 1024;
+        var terabyte = gigabyte * 1024;
+
+        if ((bytes >= 0) && (bytes < kilobyte)) {
+            return bytes + ' B';
+
+        } else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+            return (bytes / kilobyte).toFixed(precision) + ' KB';
+
+        } else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+            return (bytes / megabyte).toFixed(precision) + ' MB';
+
+        } else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+            return (bytes / gigabyte).toFixed(precision) + ' GB';
+
+        } else if (bytes >= terabyte) {
+            return (bytes / terabyte).toFixed(precision) + ' TB';
+
+        } else {
+            return bytes + ' B';
+        }
+    };
+
+    google.load('visualization', '1', {'packages': ['geochart']});
+    google.setOnLoadCallback(function () {
+
+        var toggleButtonElm = document.getElementById('toggle-button'),
+            $toggleButton = jQuery(toggleButtonElm),
+            timeSelectorElm = document.getElementById('time-slider'),
+            timeElm = document.getElementById("google-map-time"),
+            mapElm = document.getElementById('google-sec-map'),
+            bandwithElm = document.getElementById("google-map-bandwidth"),
+            chart = new google.visualization.GeoChart(mapElm),
+            data = window.cs594.data.secData,
+            shape = data.shape,
+            options = {
+                displayMode: 'markers',
+                region: "US",
+                colorAxis: {
+                    minValue: shape.min,
+                    maxValue: shape.max
+                }
+            },
+            timerId = null,
+            stepSize = (data.type === "sec") ? 1 : 60,
+            dataTable = new google.visualization.DataTable(data.table),
+            currentTime = shape.first - 1,
+            toggleStop = false,
+            // Three options, reset if
+            updateToggleButton = function () {
+                if (timeSelectorElm.max == timeSelectorElm.value) {
+                    toggleButtonElm.innerHTML = "Restart!";
+                    $toggleButton.removeClass("btn-primary");
+                    $toggleButton.removeClass("btn-warning");
+                    $toggleButton.addClass("btn-success");
+                } else if (timerId) {
+                    $toggleButton.removeClass("btn-primary");
+                    $toggleButton.addClass("btn-warning");
+                    $toggleButton.removeClass("btn-success");
+                    toggleButtonElm.innerHTML = "Stop";
+                } else {
+                    $toggleButton.addClass("btn-primary");
+                    $toggleButton.removeClass("btn-warning");
+                    $toggleButton.removeClass("btn-success");
+                    toggleButtonElm.innerHTML = "Start";
+                }
+            },
+            progressView = function () {
+
+                var dataView = new google.visualization.DataView(dataTable),
+                    totalBits = 0,
+                    aDate;
+
+                if (toggleStop) {
+                    toggleStop = false;
+                    timerId = null;
+                    updateToggleButton();
+                    return;
+                }
+
+                currentTime += stepSize;
+                timeSelectorElm.value = currentTime;
+
+                aDate = new Date(currentTime * 1000);
+                timeElm.innerHTML = aDate.toLocaleString();
+                rowIndexes = dataView.getFilteredRows([{column: 5, value: currentTime}]);
+
+                rowIndexes.forEach(function (val, index) {
+                    totalBits += parseInt(dataView.getValue(val, 2), 10);
+                });
+
+                bandwithElm.innerHTML = bytesToSize(totalBits, 1);
+
+                dataView.setRows(rowIndexes);
+                dataView.setColumns([0, 1, 2, 3]);
+                chart.draw(dataView, options);
+
+                if (currentTime < shape.last) {
+                    timerId = setTimeout(progressView, 10);
+                } else {
+                    timerId = null;
+                    updateToggleButton();
+                }
+            };
+
+        timeSelectorElm.min = shape.first;
+        timeSelectorElm.max = shape.last;
+        timeSelectorElm.step = stepSize;
+
+        progressView();
+
+        timeSelectorElm.addEventListener("change", function (e) {
+            currentTime = parseInt(timeSelectorElm.value, 10);
+            if (timerId === null) {
+                progressView();
+            }
+        }, false);
+
+
+        toggleButtonElm.addEventListener("click", function (e) {
+
+            if ($toggleButton.hasClass("btn-primary")) {
+                progressView();
+            } else if ($toggleButton.hasClass("btn-warning")) {
+                toggleStop = true;
+            } else {
+                currentTime = shape.first - 1;
+                timeSelectorElm.value = 0;
+                progressView();
+            }
+            updateToggleButton();
+        }, false);
+    });
+}());
---
+++ result_to_json.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+#Convert intermediate results into google-chart-able-json-data
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', required=True, help="Path to write Google friendly json to.")
+parser.add_argument('--input', required=True, help="Path to read intermediate json from.")
+parser.add_argument('--bin', default="minute", help="The unit of time bin to look for in data")
+parser.add_argument('--name', default="window.map", help="Global property to store resulting table in.")
+args = parser.parse_args()
+
+out_h = open(args.output, 'w')
+boiler_plate = args.name + " = {};\n"
+boiler_plate += args.name + ".type = '" + args.bin + "';\n";
+boiler_plate += args.name + """.table = {
+    cols: [{id: 'lat', label: 'Latitude', type: 'number'},
+           {id: 'long', label: 'Longitude', type: 'number'},
+           {id: 'bits', label: 'Bits', type: 'number'},
+           {id: 'size', label: 'Size', type: 'number'},
+           {id: 'ip', label: 'IP Address', type: 'string'},
+           {id: 'time', label: 'Time Bin', type: 'number'}],
+    rows: ["""
+boiler_plate_end = "]};"
+out_h.write(boiler_plate)
+
+min_bin, max_bin = None, None
+min_value, max_value = None, None
+
+with open(args.input, 'r') as in_h:
+    for a_line in in_h.xreadlines():
+        record = json.loads(a_line)
+        time = record[args.bin]
+        for source, amount in record['sources'].items():
+            if min_bin is None or time < min_bin:
+                min_bin = time
+            if max_bin is None or time > max_bin:
+                max_bin = time
+            if min_value is None or amount < min_value:
+                min_value = amount
+            if max_bin is None or amount > max_value:
+                max_value = amount
+in_h.close()
+
+with open(args.input, 'r') as in_h:
+    for a_line in in_h.xreadlines():
+        record = json.loads(a_line)
+        color = "red"
+        ip = ""
+        time = record[args.bin]
+        for source, amount in record['sources'].items():
+            color = amount
+            size = float(amount) / max_value
+            lat, lon = [float(v) for v in source.split(",")]
+            row_string = "{c: [" + ",".join(["{v: '" + str(f) + "'}" for f in [lat, lon, color, size, ip, time]]) + "]},\n"
+            out_h.write(row_string)
+
+out_h.write(boiler_plate_end)
+out_h.write("\n")
+out_h.write(args.name + ".shape = {first: " + str(min_bin) + ", last: " + str(max_bin) + ", min: " + str(min_value) + ", max: " + str(max_value) + "};\n")
