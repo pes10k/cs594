3bab89fc55839d18d3686a1e8b6f0d76a8a49889
Peter Snyder <snyderp@gmail.com>
2014-02-22 02:47:16 +0000

    -

--- temp_minute.sh
+++ temp_minute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 head -n -1 ../computed/minute_combined.txt > ../computed/minute_tmp.txt
-python -O result_to_json.py --output html/js/cs594/data/minutedata.js --input ../computed/minute_tmp.txt --name window.cs594.data.minData --group country
+python -O result_to_json.py --output html/js/cs594/data/minutedata.js --input ../computed/minute_tmp.txt --name window.cs594.data.minData --group region 
