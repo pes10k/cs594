451973f0e2ce83ed6c7ea3cd28f208b6b1b91d46
Peter Snyder <snyderp@gmail.com>
2014-02-23 23:36:36 -0600

    -

--- bandwidth_by_source.py
+++ bandwidth_by_source.py
@@ -300,5 +300,5 @@ stack.xTickLabelProperties = {
 plot = boomslang.Plot()
 plot.add(stack)
 plot.hasLegend()
-plot.setDimensions(width=1280, height=800)
+plot.setDimensions(1280, 800)
 plot.save("stackedbar.png")
